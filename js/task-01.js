662be9b3a4b4b5f82d4656e20436aac0f9619594
Marlen13 <olenamarinova@gmail.com>
2023-03-23 22:32:49 +0100

    task1,2,3

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,15 @@
-const list = document.querySelectorAll("#categories");
-//console.log(list);
+//Arr li.item
+const list = document.querySelectorAll(".item");
+//Length Arr
+const numberItem = list.length;
+console.dir(`Number of categories: ${numberItem}`);
+
+//Console h2, вкладені li
+
+list.forEach((item) => {
+  const title = item.querySelector("h2").textContent;
+  console.dir(`Category: ${title}`);
+
+  const countElem = item.querySelectorAll("li").length;
+  console.dir(`Elements: ${countElem}`);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ul = document.querySelector("#ingredients");
+const li1 = document.createElement("li");
+li1.textContent = "Potatoes";
+li1.classList.add("item");
+const li2 = document.createElement("li");
+li2.textContent = "Mushrooms";
+li2.classList.add("item");
+const li3 = document.createElement("li");
+li3.textContent = "Garlic";
+li3.classList.add("item");
+const li4 = document.createElement("li");
+li4.textContent = "Tomatos";
+li4.classList.add("item");
+const li5 = document.createElement("li");
+li5.textContent = "Herbs";
+li5.classList.add("item");
+const li6 = document.createElement("li");
+li6.textContent = "Condiments";
+li6.classList.add("item");
+ul.append(li1, li2, li3, li4, li5, li6);
